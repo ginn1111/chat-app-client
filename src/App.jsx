1530f5d787f3ea213c7f5e5eae20fb7b1dd9fbdc
ginn1111 <vanthuanjw@gmail.com>
2022-07-07 11:50:08 +0700

    base config

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App text-3xl py-6">
+      <span className="text-4xl font-bold underline">Hello reactJS</span>
+    </div>
+  );
+}
+
+export default App;
