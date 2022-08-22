21678b05a402457ab6c2519f02bf9723d84afc19
ginn1111 <vanthuanjw@gmail.com>
2022-08-22 12:59:15 +0700

    fix: img size

--- src/components/wall/FriendItem.jsx
+++ src/components/wall/FriendItem.jsx
@@ -11,7 +11,7 @@ const FriendItem = ({ avatar, name, id }) => {
         <img
           src={avatar}
           alt="avatar-friend"
-          className="w-10 h-10 sm:w-5 sm:h-5 rounded-full border-2 border-solid border-white object-center object-cover md:text-center md:mx-auto"
+          className="w-10 h-10 sm:w-5 sm:h-5 rounded-full border-2 border-solid border-white object-center object-cover md:text-center md:mx-auto flex-none"
         />
         <div className="w-full overflow-hidden md:mt-2">
           <h3 className="w-full font-[600] text-[16px] sm:text-[9px] sm:font-[500]">
